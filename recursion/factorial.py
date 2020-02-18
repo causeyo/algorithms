e86530edec7971cb7145f37724f7825e7bd93d97
causeyo <wifonik@gmail.com>
2020-02-18 17:49:28 +0100

    factorial with recursion

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,1 +1,16 @@
-pass
+"""
+FACTORIAL FUNCTION
+n! = n * (n - 1)!
+
+if n == 0, n! = 1
+
+"""
+
+
+def fact(n):
+
+    # BASE CASE
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
