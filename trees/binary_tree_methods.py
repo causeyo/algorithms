71f8f1a4480a008ecbc37aabfa34b64326024b24
causeyo <wifonik@gmail.com>
2020-03-09 19:18:25 +0100

    binary tree methods -> traversals

---
+++ trees/binary_tree_methods.py
@@ -0,0 +1,101 @@
+class BinaryTree(object):
+
+    def __init__(self, rootObj):
+
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+
+
+    def insertLeft(self, newNode):
+
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self, newNode):
+        if self.rightChild == None:
+            self.rightChild= BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self, obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
+
+    def preorder(self):
+        print(self.key)
+        if self.leftChild:
+            self.leftChild.preorder()
+        if self.rightChild:
+            self.rightChild.preorder()
+
+    def postorder(self):
+            if self.leftChild:
+                self.leftChild.postorder()
+            if self.rightChild:
+                self.rightChild.postorder()
+            print(self.key)
+
+    def inorder(self):
+        if self.leftChild:
+            self.leftChild.inorder()
+        print(self.key)
+        if self.rightChild:
+            self.rightChild.inorder()
+
+
+def preorder(tree):
+    if tree:
+        print(tree.getRootVal())
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
+
+
+def postorder(tree):
+    if tree != None:
+        postorder(tree.getLeftChild())
+        postorder(tree.getRightChild())
+        print(tree.getRootVal())
+
+
+def inorder(tree):
+    if tree != None:
+        inorder(tree.getLeftChild())
+        print(tree.getRootVal())
+        inorder(tree.getRightChild())
+
+
+r = BinaryTree('a')
+# print(r.getRootVal())
+# print(r.getLeftChild())
+r.insertLeft('b')
+r.insertLeft('c')
+r.insertRight('d')
+r.insertRight('e')
+# print(r.getLeftChild().getRootVal())
+preorder(r)
+print("\n")
+postorder(r)
+print("\n")
+inorder(r)
+print("\n")
+r.preorder()
+print("\n")
+r.inorder()
+print("\n")
+r.postorder()
+print("\n")
